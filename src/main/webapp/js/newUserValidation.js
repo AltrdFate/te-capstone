b53b4649e2a7fed0af2f287a5c32f06f95f64a9e
CJ Horsfall <cjhorsfall2018@gmail.com>
2016-11-29 17:01:02 -0500

    javascript causing redirect issues

--- src/main/webapp/js/newUserValidation.js
+++ src/main/webapp/js/newUserValidation.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-	$("#newUserForm").validate({
-	
-		debug: true,
-		rules: {
-			username: {
-				required: true,
-				minlength: 4
-			},
-			password: {
-				required: true,
-				minlength: 4
-			},
-			confirmPassword: {
-				equalTo: "#password"
-			}
-		}
-	});
-});
+//$(document).ready(function() {
+//	$("#newUserForm").validate({
+//	
+//		debug: true,
+//		rules: {
+//			username: {
+//				required: true,
+//				minlength: 4
+//			},
+//			password: {
+//				required: true,
+//				minlength: 4
+//			},
+//			confirmPassword: {
+//				equalTo: "#password"
+//			}
+//		}
+//	});
+//});
