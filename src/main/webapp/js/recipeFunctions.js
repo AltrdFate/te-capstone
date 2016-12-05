6e8b5b565aba4edaadf1a2541c32ffac07b02692
Asher Diaz <asher.diaz@gmail.com>
2016-12-05 09:50:55 -0500

    pushing code

--- src/main/webapp/js/recipeFunctions.js
+++ src/main/webapp/js/recipeFunctions.js
@@ -4,8 +4,16 @@ $(document).ready(function() {
 	    $("<p><input type='text' id='ingredients' name='ingredients' placeholder='Ingredient'/><p>").appendTo("#extraIngredients");
 	});
 	
+	
 	$("#addDirection").click(function(){
-		$("<li><input type='text' id='directions' name='directions' placeholder='Direction'/></li><p>").appendTo("#extraDirections");
+		var counter = 0;
+		if (counter <= 5) {
+			$("<li><input type='text' id='directions' name='directions' placeholder='Direction'/></li><p>").appendTo("#extraDirections");
+			counter++;
+		}
+		else {
+			alert("Join mealPlan PREMIUM to add more than 20 ingredients to your recipe.")
+		}
 	});
 	
 	$("#recipeForm").validate({
